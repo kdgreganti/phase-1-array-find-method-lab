1daa5c2b5e40dae0189fdfa273d3196ca3bb9a49
Kyle Greganti <kyle.greganti@yahoo.com>
2024-01-27 12:50:16 -0600

    meets requirements

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+
+function superbowlWin(record) {
+    let result = record.find(function(obj) {
+        return obj.result === "W";
+    });
+
+    if (result) {
+        return result.year;
+    } else {
+        return undefined;
+    }
+}
